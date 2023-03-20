302c5e19c1eea2d52ba82aae04de23e5a63398d4
Kharlamov Victor <43378234+Xapulc@users.noreply.github.com>
2023-03-20 18:17:41 +0300

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+
+def solution(x_success: int, 
+             x_cnt: int, 
+             y_success: int, 
+             y_cnt: int) -> bool:
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return ... # Ваш ответ, True или False
